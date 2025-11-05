c16e64a007868a59af8df60e2728b306c2f856ad
Wahyuach <erasedlost@gmail.com>
2025-11-05 18:59:22 +0700

    mitra midware

--- app/Http/Middleware/IsMitra.php
+++ app/Http/Middleware/IsMitra.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
-class IsAdmin
+class IsMitra
 {
     public function handle(Request $request, Closure $next): Response
     {
